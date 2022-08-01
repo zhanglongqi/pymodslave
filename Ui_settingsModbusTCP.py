728ed2d3128655c3d99af7c9556dc249c1d16c70
longqi <longqi90@gmail.com>
2022-08-01 10:39:38 +0800

    previous window is too small

--- Ui_settingsModbusTCP.py
+++ Ui_settingsModbusTCP.py
@@ -3,24 +3,29 @@
 ################################################################################
 ## Form generated from reading UI file 'settingsmodbustcp.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.1.0
+## Created by: Qt User Interface Compiler version 6.3.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QGridLayout, QLabel, QLineEdit, QSizePolicy,
+    QSpacerItem, QVBoxLayout, QWidget)
 import pyModSlaveQt_rc
 
 class Ui_SettingsModbusTCP(object):
     def setupUi(self, SettingsModbusTCP):
         if not SettingsModbusTCP.objectName():
             SettingsModbusTCP.setObjectName(u"SettingsModbusTCP")
-        SettingsModbusTCP.resize(180, 90)
-        SettingsModbusTCP.setMinimumSize(QSize(180, 90))
-        SettingsModbusTCP.setMaximumSize(QSize(180, 90))
+        SettingsModbusTCP.resize(250, 150)
+        SettingsModbusTCP.setMinimumSize(QSize(250, 150))
         icon = QIcon()
         icon.addFile(u":/img/network-16.png", QSize(), QIcon.Normal, QIcon.Off)
         SettingsModbusTCP.setWindowIcon(icon)
@@ -39,23 +44,28 @@ class Ui_SettingsModbusTCP(object):
 
         self.gridLayout.addWidget(self.lblTCPPort, 1, 0, 1, 1)
 
-        self.lblIP = QLabel(SettingsModbusTCP)
-        self.lblIP.setObjectName(u"lblIP")
-
-        self.gridLayout.addWidget(self.lblIP, 0, 0, 1, 1)
-
         self.leIP = QLineEdit(SettingsModbusTCP)
         self.leIP.setObjectName(u"leIP")
 
         self.gridLayout.addWidget(self.leIP, 0, 1, 1, 1)
 
+        self.lblIP = QLabel(SettingsModbusTCP)
+        self.lblIP.setObjectName(u"lblIP")
+
+        self.gridLayout.addWidget(self.lblIP, 0, 0, 1, 1)
+
 
         self.verticalLayout.addLayout(self.gridLayout)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
 
         self.buttonBox = QDialogButtonBox(SettingsModbusTCP)
         self.buttonBox.setObjectName(u"buttonBox")
         self.buttonBox.setOrientation(Qt.Horizontal)
         self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+        self.buttonBox.setCenterButtons(True)
 
         self.verticalLayout.addWidget(self.buttonBox)
 
@@ -74,7 +84,7 @@ class Ui_SettingsModbusTCP(object):
         SettingsModbusTCP.setWindowTitle(QCoreApplication.translate("SettingsModbusTCP", u"Modbus TCP Settings", None))
         self.leTCPPort.setText(QCoreApplication.translate("SettingsModbusTCP", u"502", None))
         self.lblTCPPort.setText(QCoreApplication.translate("SettingsModbusTCP", u"TCP Port", None))
-        self.lblIP.setText(QCoreApplication.translate("SettingsModbusTCP", u"IP", None))
         self.leIP.setInputMask(QCoreApplication.translate("SettingsModbusTCP", u"999.999.999.999;_", None))
+        self.lblIP.setText(QCoreApplication.translate("SettingsModbusTCP", u"IP", None))
     # retranslateUi
 
